50c868ce7b73f556433b8fc558806e968d9304e1
Ijalana Afolabi Isaiah <122812770+Ayolan8@users.noreply.github.com>
2024-07-01 00:08:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const currentDay = document.getElementById("current-day");
+const currentTime = document.getElementById("current-time");
+let date = new Date();
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"
+];
+
+
+
+currentDay.textContent = days[date.getDay()];
+currentTime.textContent = date.toISOString().slice(11, 19); 
